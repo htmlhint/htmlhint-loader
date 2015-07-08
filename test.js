0369272bb87b861f28955ba971fae05b27d004c1
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2015-07-08 20:21:41 +0100

    Add some basic tests

--- test.js
+++ test.js
@@ -1,7 +1,27 @@
+var chai = require('chai');
+var sinon = require('sinon');
+var sinonChai = require('sinon-chai');
+chai.use(sinonChai);
+var htmlHintLoader = require('./index');
+
 describe('htmlhint loader', function() {
 
-  it('should have some tests!', function() {
+  var prototype;
+  beforeEach(function() {
+    prototype = {
+      emitError: sinon.spy(),
+      cacheable: sinon.spy(),
+      options: {}
+    };
+  });
 
+  it('should export a function', function() {
+    chai.assert.isFunction(htmlHintLoader);
+  });
+
+  it('should be cacheable', function() {
+    htmlHintLoader.call(prototype, '');
+    chai.expect(prototype.cacheable).to.have.been.calledOnce;
   });
 
 });
