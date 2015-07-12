6c3385d339296b1f1d82641b26f27cd0ebb0b8b0
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2015-07-12 18:07:19 +0100

    Read the htmlhint file each time in case the rules have been changed (i.e. so you dont have to manually restart if using a dev server)

--- index.js
+++ index.js
@@ -5,17 +5,6 @@ var path = require('path');
 var fs = require('fs');
 var chalk = require('chalk');
 
-var HTMLHINT_CONFIG_FILE = '.htmlhintrc';
-var htmlHintConfig = HTMLHint.defaultRuleset;
-var configFile = path.join(process.cwd(), HTMLHINT_CONFIG_FILE);
-if (fs.existsSync(configFile)) {
-  try {
-    htmlHintConfig = JSON.parse(fs.readFileSync(configFile));
-  } catch (e) {
-    throw new Error('Could not parse the .htmlhintrc file!');
-  }
-}
-
 function formatMessage(message) {
 
   var evidence = message.evidence;
@@ -52,56 +41,66 @@ function defaultFormatter(messages) {
   return output.trim();
 }
 
-function lint(source, options, webpack) {
+function lint(source, options, webpack, done) {
 
-  if (options.rules) {
-    options.rules.forEach(function(rule) {
-      HTMLHint.addRule(rule);
-    });
-  }
+  try {
 
-  var report = HTMLHint.verify(source, htmlHintConfig);
-  if (report.length > 0) {
-
-    var reportByType = {
-      warning: report.filter(function(message) {
-        return message.type === 'warning'
-      }),
-      error: report.filter(function(message) {
-        return message.type === 'error'
-      })
-    };
-
-    var emitter;
-    if (options.emitAs === 'error') {
-      emitter = webpack.emitError;
-    } else if (options.emitAs === 'warning') {
-      emitter = webpack.emitWarning;
+    if (options.rules) {
+      options.rules.forEach(function(rule) {
+        HTMLHint.addRule(rule);
+      });
     }
 
-    if (reportByType.warning.length > 0) {
+    var report = HTMLHint.verify(source, options);
+    if (report.length > 0) {
 
-      var warning = options.formatter(reportByType.warning);
-      emitter ? emitter(warning) : webpack.emitWarning(warning);
+      var reportByType = {
+        warning: report.filter(function(message) {
+          return message.type === 'warning'
+        }),
+        error: report.filter(function(message) {
+          return message.type === 'error'
+        })
+      };
 
-    }
+      var emitter;
+      if (options.emitAs === 'error') {
+        emitter = webpack.emitError;
+      } else if (options.emitAs === 'warning') {
+        emitter = webpack.emitWarning;
+      }
 
-    if (reportByType.error.length > 0) {
+      if (reportByType.warning.length > 0) {
 
-      var error = options.formatter(reportByType.error);
-      emitter ? emitter(error) : webpack.emitError(error);
+        var warning = options.formatter(reportByType.warning);
+        emitter ? emitter(warning) : webpack.emitWarning(warning);
 
-      if (options.failOnError) {
-        throw new Error('Module failed because of a htmlhint error.');
+      }
+
+      if (reportByType.error.length > 0) {
+
+        var error = options.formatter(reportByType.error);
+        emitter ? emitter(error) : webpack.emitError(error);
+
+        if (options.failOnError) {
+          throw new Error('Module failed because of a htmlhint error.');
+        }
+
+      }
+
+      if (reportByType.warning.length > 0 && options.failOnWarning) {
+        throw new Error('Module failed because of a htmlhint warning.');
       }
 
     }
-    
-    if (reportByType.warning.length > 0 && options.failOnWarning) {
-      throw new Error('Module failed because of a htmlhint warning.');
-    }
 
+    done(null, source);
+
+  } catch (e) {
+    done(e);
   }
+
+
 }
 
 module.exports = function(source) {
@@ -112,17 +111,46 @@ module.exports = function(source) {
       emitAs: null, //can be either warning or error
       failOnError: false,
       failOnWarning: false,
-      rules: []
+      rules: [],
+      configFile: '.htmlhintrc'
     },
-    htmlHintConfig, //htmlhint default rules
     this.options.htmlhint || {}, // user defaults
     loaderUtils.parseQuery(this.query) // loader query string
   );
 
   this.cacheable();
 
-  lint(source, options, this);
+  var done = this.async();
+  var webpack = this;
 
-  return source;
+  var configFilePath = path.join(process.cwd(), options.configFile);
+
+  fs.exists(configFilePath, function(exists) {
+
+    if (exists) {
+
+      fs.readFile(configFilePath, function(err, configString) {
+
+        if (err) {
+          return done(err);
+        }
+
+        try {
+          var htmlHintConfig = JSON.parse(configString);
+        } catch (e) {
+          done(new Error('Could not parse the htmlhint config file'));
+        }
+
+        lint(source, assign(options, htmlHintConfig), webpack, done);
+
+      });
+
+    } else {
+
+      lint(source, assign(options, HTMLHint.defaultRuleset), webpack, done);
+
+    }
+
+  });
 
 };
